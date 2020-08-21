dea8a8e1bf8e3a116fed6240cb8391702fd48987
Fuji <ray2freelife@gmail.com>
2020-08-21 11:27:38 +0600

    test travis

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
-          Its working!
+          Its working!!!
         </p>
         <a
           className="App-link"
